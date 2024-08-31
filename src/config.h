f26fec3fcb82adb1d32ff152bdf5cded5efc42f3
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-08-31 17:02:57 +0200

    configurind dependencies

--- src/config.h
+++ src/config.h
@@ -5,9 +5,15 @@
 #ifdef _DEBUG
 #include "dependencies/glad/glad.h"
 #include "_deps/glfw-src/include/GLFW/glfw3.h"
+#include "dependencies/imgui/imgui.h"
+#include "dependencies/imgui/imgui_impl_glfw.h"
+#include "dependencies/imgui/imgui_impl_opengl3.h"
 #else
 #include <glad/glad.h>  // OpenGL, has to be before glfw
 #include <GLFW/glfw3.h> // Windowing stuff
+#include <imgui/imgui.h>  // UI stuff
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h> 
 #endif
 
 // Reading from files
@@ -16,3 +22,6 @@
 #include <string>
 
 #include <vector>
+
+#include <functional>
+#include <filesystem>

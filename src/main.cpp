295ced32fd1387cc59a5b93e07d0b998c0c7dbfc
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-09-03 18:52:28 +0200

    Making sure that the program uses a dedicated GPU i.e Nvidia GPU. Can be commented out

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "guiApplication.hpp"
 
+
 using namespace ImGui;
 
+// Use Nvidia dedicated GPU
+extern "C" {
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+}
+    
 int main()
 {
     GuiApplication app;

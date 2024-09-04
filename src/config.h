48fbf6dc01b9c54d14594bb790b67fe5d3c7c0d5
Artistic-Nyungu <brightmakanduchauke@gmail.com>
2024-09-04 13:03:44 +0200

    Adding image loader library

--- src/config.h
+++ src/config.h
@@ -2,18 +2,23 @@
 
 #include <iostream>
 
+// Image loader library
+#define STB_IMAGE_IMPLEMENTATION
+
 #ifdef _DEBUG
 #include "dependencies/glad/glad.h"
 #include "_deps/glfw-src/include/GLFW/glfw3.h"
 #include "dependencies/imgui/imgui.h"
 #include "dependencies/imgui/imgui_impl_glfw.h"
 #include "dependencies/imgui/imgui_impl_opengl3.h"
+#include "dependencies/stb/stb_image.h"
 #else
 #include <glad/glad.h>  // OpenGL, has to be before glfw
 #include <GLFW/glfw3.h> // Windowing stuff
 #include <imgui/imgui.h>  // UI stuff
 #include <imgui/imgui_impl_glfw.h>
 #include <imgui/imgui_impl_opengl3.h> 
+#include <stb/stb_image.h>
 #endif
 
 // Reading from files
@@ -21,7 +26,4 @@
 #include <sstream>
 #include <string>
 
-#include <vector>
-
-#include <functional>
 #include <filesystem>
